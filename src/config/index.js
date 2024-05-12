5b02b0f362695b3e751fe146e6980a7a043d7c90
Anil Ozsoy <anilozsoy@gmail.com>
2024-05-12 19:00:55 +0200

    🔧 (config/index.js): update import statement for auth from "express-openid-connect" to { auth } to match the named export in the module

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 import { app, router } from "./express.js";
-import auth from "express-openid-connect";
+import { auth } from "express-openid-connect";
 import prisma from "./db.js";
 import dotenv from "dotenv";
 export { prisma, dotenv, app, router, auth };
