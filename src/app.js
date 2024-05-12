4850b564d5e8510ffe1d678f82efd75aebbbd7ae
Anil Ozsoy <anilozsoy@gmail.com>
2024-05-12 18:59:58 +0200

    🔧 (app.js): refactor import statements to use named imports for better readability and maintainability ✨ (app.js): add support for routing to the authRouter on the root path ("/") to handle authentication requests ⬆️ (app.js): upgrade the default port to use the value from the process.env.PORT environment variable if available, otherwise fallback to 3000

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import express from "express";
-import dotenv from "dotenv";
+import { app, dotenv } from "./config/index.js";
+import { authRouter } from "./api/index.js";
 dotenv.config();
-
-const app = express();
+app.use("/", authRouter);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
