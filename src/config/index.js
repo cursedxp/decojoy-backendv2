073aad38dc39ebd2e604c2cbd4a730603ba53687
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-16 11:02:27 +0200

    ♻️ (config/index.js): reorder import statements to improve readability and maintainability ✨ (config/index.js): add import statements for Schema and model from mongoose to be able to use them in the codebase

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
-import { app, router } from "./express.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-export { dotenv, app, router, mongoose };
+import { Schema, model } from "mongoose";
+import { app, router } from "./express.js";
+export { dotenv, app, router, mongoose, Schema, model };
