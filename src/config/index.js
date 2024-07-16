3fca4ab92d9cf57a8fdbb4e08d2937c890d4434a
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-16 10:26:10 +0200

    ♻️ (config/index.js): refactor imports and exports to improve code organization and remove unused imports 🔧 (config/index.js): update imports and exports to use mongoose instead of prisma for database operations

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
 import { app, router } from "./express.js";
-import { auth } from "express-openid-connect";
-import prisma from "./db.js";
 import dotenv from "dotenv";
-export { prisma, dotenv, app, router, auth };
+import mongoose from "mongoose";
+export { dotenv, app, router, mongoose };
