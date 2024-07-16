361ffcee0e490b1385606f897b1b7810c02d0999
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-16 16:10:38 +0200

    ♻️ (config/index.js): refactor import statements to improve readability and remove unnecessary imports

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,4 @@
+import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import { Schema, model } from "mongoose";
-import { app, router } from "./express.js";
-export { dotenv, app, router, mongoose, Schema, model };
+export { mongoose, express, dotenv };
