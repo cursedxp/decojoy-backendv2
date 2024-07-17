54a8e54a1f45310aaa238cf5acc6ee37fc94ad7a
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-17 13:25:06 +0200

    ✨ (style.controller.js): add controller functions to handle CRUD operations for styles 📝 (style.controller.js): add documentation comments for each controller function to improve code readability and maintainability

---
+++ src/controllers/style.controller.js
@@ -0,0 +1,69 @@
+import { mongoose } from "../config/index.js";
+import { Style } from "../models/index.js";
+
+//Get all styles
+const getAllStyles = async (req, res) => {
+  try {
+    const styles = await Style.find().sort({ createdAt: -1 });
+    res.status(200).json(styles);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//Get a single style
+const getStyle = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ message: "No style with that id" });
+
+  try {
+    const style = await Style.findById(id);
+    res.status(200).json(style);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//Create a style
+const createStyle = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const newStyle = await Style.create({ name });
+    res.status(200).json(newStyle);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//Update a style
+const updateStyle = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+    const updatedStyle = await Style.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true }
+    );
+    res.status(200).json(updatedStyle);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+//Delete a style
+const deleteStyle = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ message: "No style with that id" });
+  try {
+    await Style.findByIdAndDelete(id);
+    res.status(200).json({ message: "Style deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export { getAllStyles, getStyle, createStyle, updateStyle, deleteStyle };
