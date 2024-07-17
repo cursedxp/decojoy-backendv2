3cb99bb6b2b9e82c668329035eb52dac871aecbb
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-17 10:52:57 +0200

    📝 (room.model.js): add unique constraint to the name field in the room schema to ensure uniqueness of room names 📝 (style.model.js): add timestamps option to the style schema to automatically add createdAt and updatedAt fields to style documents

--- src/models/room.model.js
+++ src/models/room.model.js
@@ -1,11 +1,15 @@
 import { Schema, model } from "mongoose";
 
-const roomSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const roomSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Room = model("Room", roomSchema);
 export default Room;
--- src/models/style.model.js
+++ src/models/style.model.js
@@ -1,11 +1,14 @@
 import { Schema, model } from "mongoose";
 
-const styleSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const styleSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Style = model("Style", styleSchema);
 export default Style;
