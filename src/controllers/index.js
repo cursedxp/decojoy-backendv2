eb17a807a8144934528b9983e5a2354846126e97
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-18 11:42:17 +0200

    ✨ (index.js): add productController to the list of exported controllers to make it accessible in other modules

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,10 @@
 import * as roomController from "./room.controller.js";
 import * as styleController from "./style.controller.js";
 import * as conceptController from "./concept.controller.js";
-export { roomController, styleController, conceptController };
+import * as productController from "./product.controller.js";
+export {
+  roomController,
+  styleController,
+  conceptController,
+  productController,
+};
