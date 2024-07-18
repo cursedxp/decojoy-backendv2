ac98c34b7c5d64d7fdcd66cc3d329f313403aeb4
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-18 10:51:43 +0200

    ✨ (index.js): add conceptRoutes to the routes array to include routes for concepts in the application

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 import roomRoutes from "./room.route.js";
 import styleRoutes from "./style.route.js";
+import conceptRoutes from "./concept.route.js";
 
 const routes = [
   { path: "rooms", router: roomRoutes },
   {
     path: "styles",
     router: styleRoutes,
+  },
+  {
+    path: "concepts",
+    router: conceptRoutes,
   },
 ];
 
