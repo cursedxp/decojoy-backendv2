5fde0b79f8682c3cf222730183ebd4e8ffc773ff
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-18 11:42:43 +0200

    ✨ (product.route.js): create a new file for handling product routes 📝 (product.route.js): add routes for getting all products, getting a product by id, creating a product, updating a product, and deleting a product

---
+++ src/routes/product.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { productController } from "../controllers/index.js";
+const router = Router();
+
+//Get all products
+router.get("/", productController.getAllProducts);
+
+//Get a product by id
+router.get("/:id", productController.getProductById);
+
+//Create a product
+router.post("/", productController.createProduct);
+
+//Update a product
+router.put("/:id", productController.updateProduct);
+
+//Delete a product
+router.delete("/:id", productController.deleteProduct);
+
+export default router;
