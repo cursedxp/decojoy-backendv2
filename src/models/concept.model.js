a0f3ec12a5daa0e9c6643e9f0e505a2e3d45f9f1
Anil Ozsoy <anilozsoy@gmail.com>
2024-07-30 08:30:15 +0200

    📝 (concept.model.js): reformat conceptSchema to improve readability and maintainability 📝 (product.model.js): reformat productSchema to improve readability and maintainability

--- src/models/concept.model.js
+++ src/models/concept.model.js
@@ -1,52 +1,55 @@
 import { Schema, model } from "mongoose";
 
-const conceptSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  overview: {
-    type: String,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: true,
-  },
-  images: {
-    type: [String],
-    required: true,
-  },
-  colors: {
-    type: [String],
-    required: true,
-  },
-  like: {
-    type: Number,
-    default: 0,
-  },
-  roomType: {
-    type: Schema.Types.ObjectId,
-    ref: "Room",
-    required: true,
-  },
-  roomStyle: {
-    type: Schema.Types.ObjectId,
-    ref: "Style",
-    required: true,
-  },
-  products: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Product",
+const conceptSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-  published: {
-    type: Boolean,
-    default: false,
+    overview: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    like: {
+      type: Number,
+      default: 0,
+    },
+    roomType: {
+      type: Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+    roomStyle: {
+      type: Schema.Types.ObjectId,
+      ref: "Style",
+      required: true,
+    },
+    products: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    published: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Concept = model("Concept", conceptSchema);
 export default Concept;
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,31 +1,50 @@
 import { Schema, model } from "mongoose";
 
-const productSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    price: {
+      type: Number,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    like: {
+      type: Number,
+      default: 0,
+    },
+    dimentions: {
+      type: String,
+      required: false,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    category: {
+      type: String,
+      required: false,
+    },
+    colors: {
+      type: [String],
+      required: false,
+    },
+    published: {
+      type: Boolean,
+      default: false,
+    },
   },
-  price: {
-    type: Number,
-    required: false,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  like: {
-    type: Number,
-    default: 0,
-  },
-  dimentions: {
-    type: String,
-    required: false,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 const Product = model("Product", productSchema);
 export default Product;
