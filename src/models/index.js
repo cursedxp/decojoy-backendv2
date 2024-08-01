3e617c498a5696609ef091ff107087994a913dd2
Anil Ozsoy <anilozsoy@gmail.com>
2024-08-01 11:24:51 +0200

    📝 (models/index.js): update import statement for ProductCategory model to use correct file name ♻️ (models/index.js): refactor import statement for ProductCategory model to use correct file name for better code organization and consistency

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,6 @@ import Room from "./room.model.js";
 import Style from "./style.model.js";
 import Concept from "./concept.model.js";
 import Product from "./product.model.js";
-import ProductCategory from "./productCategory.js";
+import ProductCategory from "./productCategory.model.js";
 
 export { Room, Style, Concept, Product, ProductCategory };
