bdb0acf6f8d151c15ea032150ffe73ef1d2a4848
Anil Ozsoy <anilozsoy@gmail.com>
2024-08-27 10:41:42 +0200

    ✨ (models/index.js): add User model to the exported models 📝 (models/index.js): update documentation to reflect the addition of the User model

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,6 @@ import Style from "./style.model.js";
 import Concept from "./concept.model.js";
 import Product from "./product.model.js";
 import ProductCategory from "./productCategory.model.js";
+import User from "./user.model.js";
 
-export { Room, Style, Concept, Product, ProductCategory };
+export { Room, Style, Concept, Product, ProductCategory, User };
