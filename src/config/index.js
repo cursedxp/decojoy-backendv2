3035b9972d019c73875ad281626a7bb6ac51684f
Anil Ozsoy <anilozsoy@gmail.com>
2024-08-27 11:17:30 +0200

    ♻️ (config/index.js): remove unused import of argon2 library to clean up the code and improve maintainability

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import argon2 from "argon2";
 import jwt from "jsonwebtoken";
 import helmet from "helmet";
-export { mongoose, express, dotenv, cors, argon2, jwt, helmet };
+export { mongoose, express, dotenv, cors, jwt, helmet };
