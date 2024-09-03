9c50ab8bcb4c3cbadbdb8391a428b78746001fab
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-03 10:18:54 +0200

    📝 (user.model.js): add trim and lowercase options to the name and email fields for better data consistency and validation ♻️ (user.model.js): refactor the structure of the likes field to separate products and concepts into separate arrays for better organization and readability ✨ (user.model.js): add timestamps option to automatically add createdAt and updatedAt fields to the user model for easier tracking of user data changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,65 +1,72 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-    default: "",
-  },
-  concepts: {
-    type: [String],
-    default: [],
-  },
-  likes: {
-    products: [
-      {
-        product: {
-          type: Schema.Types.ObjectId,
-          ref: "Product",
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    concepts: {
+      type: [String],
+      default: [],
+    },
+    likes: {
+      products: [
+        {
+          product: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+          },
+          likedAt: {
+            type: Date,
+            default: Date.now,
+          },
         },
-        likedAt: {
-          type: Date,
-          default: Date.now,
+      ],
+      concepts: [
+        {
+          concept: {
+            type: Schema.Types.ObjectId,
+            ref: "Concept",
+          },
+          likedAt: {
+            type: Date,
+            default: Date.now,
+          },
         },
-      },
-    ],
-    concepts: [
-      {
-        concept: {
-          type: Schema.Types.ObjectId,
-          ref: "Concept",
-        },
-        likedAt: {
-          type: Date,
-          default: Date.now,
-        },
-      },
-    ],
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin", "proUser"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "suspended"],
+      default: "active",
+    },
   },
-  role: {
-    type: String,
-    enum: ["user", "admin", "proUser"],
-    default: "user",
-  },
-  status: {
-    type: String,
-    enum: ["active", "inactive", "suspended"],
-    default: "active",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 export default User;
