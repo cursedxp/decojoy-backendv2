a54f618ff7d23d04380d15b3fba90286b00a401e
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-05 15:45:37 +0200

    🔧 (app.js): update cors configuration to allow specific origins, credentials, methods, and headers The cors configuration has been updated to allow requests from the origin specified in the FRONTEND_URL environment variable or "http://localhost:3000" if the variable is not set. The configuration now includes support for credentials, specific methods (GET, POST, PUT, DELETE), and allowed headers (Content-Type, Authorization). This change improves security and flexibility in handling cross-origin requests.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
