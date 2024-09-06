e4cc2ca53793044b6ce76ce01debbf275f3ce5d1
Anil Ozsoy <anilozsoy@gmail.com>
2024-09-06 15:33:14 +0200

    📝 (auth.js): remove unnecessary comments and improve code readability The unnecessary comments have been removed to improve code readability. The code now focuses on the essential logic without unnecessary explanations.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,29 +2,23 @@ import { jwt } from "../config/index.js";
 import { User } from "../models/index.js";
 
 const auth = async (req, res, next) => {
-  // Extract the token from the Authorization header
   const token = req.cookies.auth;
 
-  // Check if the token exists
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   try {
-    // Verify the token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Set the user ID
     req.userId = decoded.id;
 
-    // Fetch the user from the database to get the role
     const user = await User.findById(decoded.id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
     req.userRole = user.role;
 
-    // Call the next middleware
     next();
   } catch (error) {
     // Log the error for debugging
